cd266c8c0e9adad7b0b5869152d116c5a289723b
zhen-zorin <zhen.zorin@gmail.com>
2024-10-23 14:55:52 +0700

    Add files via upload

---
+++ varibls.py
@@ -0,0 +1,6 @@
+namber_homerwork = 12 # количество сделаных домашних заданий
+namber_hour = 1.5 # количество затраченных часов
+name_course = 'Python' # название курса
+tiem_for_one = namber_hour / namber_homerwork # Среднее время выполнения
+print('Курс:',name_course,',' ,'всего задач:' , namber_homerwork ,',', 'затрачено часов:',namber_hour,',',
+      'среднее время выполнения ', tiem_for_one ,'часа.')

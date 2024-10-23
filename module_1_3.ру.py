2d63f5c965e6f68972433b9e0f438c317c827d95
zhen-zorin <zhen.zorin@gmail.com>
2024-10-23 14:35:27 +0700

    Add files via upload

---
+++ module_1_3.ру.py
@@ -0,0 +1,9 @@
+name = 'Евгений'
+print('Имя:', name)
+age = 52
+print('Возраст:'+ str(age))
+nuw_age = age + 100
+print("Новая эра:" , nuw_age)
+is_student = 'Является студентом:'
+name = bool (is_student)
+print(is_student , name)

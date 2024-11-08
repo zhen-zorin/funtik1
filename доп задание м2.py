f8c23f05f9212963f267e9fe5b2b826808021b6b
zhen-zorin <zhen.zorin@gmail.com>
2024-11-08 15:51:22 +0700

    Add files via upload
    дополнительное задание по модулю 2

---
+++ доп задание м2.py
@@ -0,0 +1,25 @@
+
+list_ = []
+n = int(input('Введите число: '))
+del_ = []
+c = 3
+b = 1
+a = 1
+i = 1
+j = 1
+while c < n:
+    if n % c == 0 :
+        del_.append(c)
+    c += 1
+for i in del_:
+    for j in range (1, i):
+        for k in range(1, i // 2 + 1):
+            if k + j == i and k != j:
+                list_.append([k, j])
+for b in range(1, n):
+    for a in range(1, n // 2+1):
+        if b + a == n and a != b:
+            list_.append([ a, b])
+            continue
+result = sorted(list_)
+print(*result)

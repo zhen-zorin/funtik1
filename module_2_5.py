94bf3426fe0fb5d3c1a8f20be8dfe1851cf44fb7
zhen-zorin <zhen.zorin@gmail.com>
2024-11-21 11:56:17 +0700

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -1,14 +1,15 @@
 def get_matrix ():
     matrix = []
     a = []
-    n = int(input('строки: '))
-    m = int(input('столбцы: '))
-    value = int(input('Число: '))
     for i in range(0, n) :
         for j in range(0,m) :
             a.append(value)
         matrix.append(a)
         a = []
     return matrix
-win = get_matrix()
-print(*win)
+win1 = get_matrix(2,2,10)
+win2 = get_matrix(3,5,42)
+win3 = get_matrix(4,2,13)
+print(*win1)
+print(*win2)
+print(*win3)

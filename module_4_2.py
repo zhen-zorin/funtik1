e0e936e1096433f17088df968b2593561bc68b67
zhen-zorin <zhen.zorin@gmail.com>
2024-11-22 11:09:43 +0700

    Add files via upload
    Домашняя работа по уроку "Пространство имен."

---
+++ module_4_2.py
@@ -0,0 +1,7 @@
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции "test_function"')
+    inner_function()
+#inner_function()
+c = test_function()
+print(c)

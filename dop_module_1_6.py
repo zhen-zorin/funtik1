28d1f70ec58a1b865d881ab95b53601f382f1629
zhen-zorin <zhen.zorin@gmail.com>
2024-11-26 14:33:09 +0700

    Update dop_module_1_6.py

--- dop_module_1_6.py
+++ dop_module_1_6.py
@@ -1,12 +1,18 @@
 grades_ = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
-grades_sum = [sum(grades_[0]),sum(grades_[1]),sum(grades_[2]),sum(grades_[3]),sum(grades_[4])]
-grades_1 = [ grades_sum[0] / len(grades_[0])]
-grades_2 = [grades_sum[1] / len(grades_[1])]
-grades_3 = [grades_sum[2] / len(grades_[2])]
-grades_4 = [grades_sum[3] / len(grades_[3])]
-grades_5 = [grades_sum[4] / len(grades_[4])]
-students = {'Jonny','Bilbo','Steve','khendrik','Aaron'}
-students_1 = list({'Jonny','Bilbo','Steve','khendrik','Aaron'})
+grades_new =[]
+grades_1 = [sum(grades_[0]) / len(grades_[0])]
+grades_new.append(grades_1[0])
+grades_2 = [sum(grades_[1]) / len(grades_[1])]
+grades_new.append(grades_2[0])
+grades_3 = [sum(grades_[2]) / len(grades_[2])]
+grades_new.append(grades_3[0])
+grades_4 = [sum(grades_[3]) / len(grades_[3])]
+grades_new.append(grades_4[0])
+grades_5 = [sum(grades_[4]) / len(grades_[4])]
+grades_new.append(grades_5[0])
+students = {'Jonny','Bilbo','Steve','Khendrik','Aaron'}
+students_1 = list(students)
 students_1.sort()# сортируем список учеников по алфавиту
-delta_ = {students_1[0]: grades_1, students_1[1] :grades_2, students_1[2]: grades_3, students_1[3]:grades_4, students_1[4]:grades_5}
+delta_ = dict(zip(students_1, grades_new))
+
 print(delta_)
